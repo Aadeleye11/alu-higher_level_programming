b54e0dac61595eb019a62eb467e47d12b71a8c79
Aadeleye11 <a.adeleye@alustudent.com>
2025-02-24 19:31:08 -0800

    Abeeg

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_comb3():
+    result = []
     for i in range(0, 9):
         for j in range(i + 1, 10):
-            if i == 8 and j == 9:
-                print(f"{i}{j}")
-            else:
-                print(f"{i}{j}", end=", ")
+            result.append(f"{i}{j}")
+    print(", ".join(result)
